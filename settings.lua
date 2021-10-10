aaf1cbe51f57c1f484dd49b34f40a4c11c7ced35
jodokus31 <33641866+jodokus31@users.noreply.github.com>
2021-10-10 20:06:25 +0200

    additional settings, logging, optimization, docu

--- settings.lua
+++ settings.lua
@@ -3,18 +3,18 @@ local settings = {}
 table.insert(settings,
     {
         type = "int-setting",
-        name = "autobuild-update-period",
+        name = "autobuild-cycle-length-in-ticks",
         order = "a",
         setting_type = "runtime-global",
-        default_value = 10,
+        default_value = 10,  --1/6 sec.
         minimum_value = 1,
-        maximum_value = 600,
+        maximum_value = 600, --10sec.
     })
 
 table.insert(settings,
     {
         type = "int-setting",
-        name = "autobuild-action-rate",
+        name = "autobuild-actions-per-cycle",
         order = "b",
         setting_type = "runtime-per-user",
         default_value = 2,
@@ -25,10 +25,10 @@ table.insert(settings,
 table.insert(settings,
     {
         type = "int-setting",
-        name = "autobuild-update-threshold",
+        name = "autobuild-move-latency",
         order = "c",
         setting_type = "runtime-per-user",
-        default_value = 1,
+        default_value = 0,
         minimum_value = 0,
         maximum_value = 50,
     })
@@ -36,12 +36,35 @@ table.insert(settings,
 table.insert(settings,
     {
         type = "int-setting",
-        name = "autobuild-idle-rebuild",
+        name = "autobuild-move-threshold",
         order = "d",
         setting_type = "runtime-per-user",
-        default_value = 30,
+        default_value = 0,
+        minimum_value = 0,
+        maximum_value = 100,
+    })
+
+table.insert(settings,
+    {
+        type = "int-setting",
+        name = "autobuild-idle-cycles-before-recheck",
+        order = "e",
+        setting_type = "runtime-per-user",
+        default_value = 12,
         minimum_value = 1,
         maximum_value = 100,
+    })
+
+
+table.insert(settings,
+    {
+        type = "int-setting",
+        name = "autobuild-log-level",
+        order = "z",
+        setting_type = "runtime-global",
+        default_value = 0, 
+        minimum_value = 0,
+        maximum_value = 5, 
     })
 
 data:extend(settings)
